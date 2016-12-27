d636c4ac60990d030f07fadcfbf59f14cb0a7616
root <gagesaunders@hotmail.com>
2016-12-27 12:16:23 -0500

    Add handlers for 404 and 500

--- site_scan/views.py
+++ site_scan/views.py
@@ -90,3 +90,16 @@ def cidr(request):
 		p = subprocess.Popen(command , shell=True, stdout=subprocess.PIPE)
 		scan = get_object_or_404(SiteScan, pk=id)
 		return redirect('/o_saint/%s/' % id)
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
